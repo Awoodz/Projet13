0f9156e3062ba090bbdd62cd6e3042af75322178
Awoodz <a.bois77@gmail.com>
2020-11-11 18:27:49 +0100

    heroku push

--- mycoldmanager/urls.py
+++ mycoldmanager/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include, url
+import debug_toolbar
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns = [url(r"^__debug__/", include(debug_toolbar.urls)),] + urlpatterns
